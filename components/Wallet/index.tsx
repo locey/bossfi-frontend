a800c2d9b4c820e416b98324d347e0de1d5818ed
king <iforcecome@gmail.com>
2025-07-04 09:22:53 +0800

    feat: update

--- components/Wallet/index.tsx
+++ components/Wallet/index.tsx
@@ -7,7 +7,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { mainnet, sepolia } from 'wagmi/chains'
 
 const config = getDefaultConfig({
-  appName: 'Web3 面试题平台',
+  appName: 'My Web3 App',
   projectId: 'YOUR_WALLETCONNECT_PROJECT_ID', // 可前往 https://cloud.walletconnect.com 注册
   chains: [mainnet, sepolia],
   ssr: false,
